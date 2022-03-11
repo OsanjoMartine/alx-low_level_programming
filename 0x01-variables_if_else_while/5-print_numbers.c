0132f76247fb237343d8a8fc40afa33ce0845f21
OsanjoMartine <osanjomartine@gmail.com>
2022-03-11 13:54:21 -0800

    base 10 members

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 (Successs)
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (0 <= 9)
+	{
+		put(n);
+		n++;
+	}
+
+	return (0);
+}
