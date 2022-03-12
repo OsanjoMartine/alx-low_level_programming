8bbca3aba796879c4bbf315b1110525abe5ccc36
OsanjoMartine <osanjomartine@gmail.com>
2022-03-11 16:11:13 -0800

    3 peat

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 (Successs)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = i; j <= 9; j++)
+		{
+			for (k = j; k <= 9; k++)
+			{
+				if (i == j && k == j)
+				{
+					continue;
+				}
+
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+
+				if (i == 8 && j == 9 && k == 9)
+				{
+					continue;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
