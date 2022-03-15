ee249dadcc86743d7da1408496233b29e39f0665
OsanjoMartine <osanjomartine@gmail.com>
2022-03-15 15:49:36 -0700

    absolutre

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the sign of the number entered
+ *@n: variable that could be any character
+ *
+ * Return: 1 (Success)
+ */
+int print_last_digit(int n)
+{
+	int i = (int)n;
+
+	return (n % 10);
+}
