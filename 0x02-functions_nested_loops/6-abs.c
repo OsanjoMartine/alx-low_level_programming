491fdd7eb909582b4b86d8bfbef2dc8d245599bd
OsanjoMartine <osanjomartine@gmail.com>
2022-03-15 15:46:11 -0700

    absolutre

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * int _abs- prints the sign of the number entered
+ *  _abs- prints the sign of the number entered
  *@n: variable that could be any character
  *
  * Return: 1 (Success)
  */
-int _abs(int n);
+int _abs(int n)
 {
 	int i = (int)n;
 
