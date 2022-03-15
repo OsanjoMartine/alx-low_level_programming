4977ec88987d8db832c3c9c10c32b8677501be8d
OsanjoMartine <osanjomartine@gmail.com>
2022-03-15 15:39:08 -0700

    absolutre

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * int _abs- prints the sign of the number entered
+ *@n: variable that could be any character
+ *
+ * Return: 1 (Success)
+ */
+int int _abs(int n);
+{
+	int i = (int)n;
+
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (i == 0)
+	{
+		return (n);
+	}
+
+	return (n * -1);
+}
