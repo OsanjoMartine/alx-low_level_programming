15c2bd32abbe7c691699c167659d0149a13a6e5f
OsanjoMartine <99840191+OsanjoMartine@users.noreply.github.com>
2022-03-16 02:52:36 +0300

    nines table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * times_table(void) - times table 9
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int x, y, product;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+            product = x * y;
+            if (product > 9)
+            {
+                _putchar (product / 10 + '0');
+                _putchar (product % 10 + '0');
+            }
+            else
+            {
+                _putchar (' ');
+                _putchar (product + '0');
+            }
+            if (y != 9)
+            {
+                _putchar (',');
+                _putchar (' ');
+            }
+		}
+			_putchar ('\n');
+	}
+}
