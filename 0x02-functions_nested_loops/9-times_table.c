7cc7b6ac6f66eaecb6de3685e501a7cd5ac0c57d
OsanjoMartine <99840191+OsanjoMartine@users.noreply.github.com>
2022-03-16 02:59:00 +0300

    nines table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,29 +7,38 @@
  */
 void times_table(void)
 {
-	int x, y, product;
+	int x;
+	int y;
+	int prod;
 
-	for (x = 0; x <= 9; x++)
+	for (y = 0; y <= 9; y++)
 	{
-        for (y = 0; y <= 9; y++)
-        {
-            product = x * y;
-            if (product > 9)
-            {
-                _putchar (product / 10 + '0');
-                _putchar (product % 10 + '0');
-            }
-            else
-            {
-                _putchar (' ');
-                _putchar (product + '0');
-            }
-            if (y != 9)
-            {
-                _putchar (',');
-                _putchar (' ');
-            }
-        }
-        _putchar ('\n');
-    }
+		_putchar('0');
+		for (x = 1; x <= 9; x++)
+		{
+			prod = x * y;
+			if (prod <= 9)
+			{
+				if (x <= 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(prod + '0');
+				}
+			}
+			else
+			{
+				if (x <= 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(prod / 10 + '0');
+					_putchar(prod % 10 + '0');
+				}
+			}
+			if (x == 9)
+			_putchar('\n');
+		}
+	}
 }
