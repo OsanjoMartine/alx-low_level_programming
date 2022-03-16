7a54b5eef528d12b8124ed23317ca808d9c985d9
OsanjoMartine <99840191+OsanjoMartine@users.noreply.github.com>
2022-03-16 22:49:12 +0300

    100-times table n

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,75 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_times_table - prints the n table of n passed
+ * @n: this is a parameter passed for the table
+ *
+ * Return: 0 (Success)
+ */
+void print_times_table(int n)
+{
+	int i;
+	int j;
+	int prod;
+
+	if (n > 15 || n < 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			prod = i * j;
+			if (prod > 99)
+			{
+				if (j != 0)
+				{
+					_putchar(' ');
+				}
+				_putchar((prod / 100) % 10 + '0');
+				_putchar((prod / 10) % 10 + '0');
+				_putchar(prod % 10 + '0');
+			}
+			else if (prod > 9)
+			{
+				if (j == 0)
+				{
+					_putchar((prod / 10) % 10 + '0');
+					_putchar(prod % 10 + '0');
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((prod / 10) % 10 + '0');
+					_putchar(prod % 10 + '0');
+
+				}
+			}
+			else
+			{
+				if (j == 0)
+				{
+					_putchar(prod % 10 + '0');
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(prod % 10 + '0');
+				}
+			}
+			if (j != n)
+			{
+				_putchar(',');
+			}
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
