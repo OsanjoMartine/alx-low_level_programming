5386dabc9e1462f7206de6b363dd21085a4259eb
OsanjoMartine <99840191+OsanjoMartine@users.noreply.github.com>
2022-03-16 23:17:45 +0300

    natural numbers divisibl;e by 3 or 5 upto 1023

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,24 +1,35 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * print_sum_multiples_1024 - prints the sum of natural numbers divisible by 3 or 5 upto 1024
+ *
+ * Return: 0 (Success)
+ */
 void print_sum_multiples_1024(void)
 {
-    int i;
-    int sum = 0;
-    for (i = 0; i < 1024; i++)
-    {
-        if (i % 3 == 0 || i % 5 == 0)
-        {
-            sum += i;
-        }
-    }
+	int i;
+	int sum = 0;
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
 
-    printf("%d", sum);
+	printf("%d", sum);
+	_putchar('\n');
 }
 
+/**
+ * main - runs the code
+ *
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-    print_sum_multiples_1024();
+	print_sum_multiples_1024();
 
-    return (0);
+	return (0);
 }
