2e2268fabe78e2250d518746c3c367258e6d4df8
OsanjoMartine <99840191+OsanjoMartine@users.noreply.github.com>
2022-03-16 03:55:12 +0300

    to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		if (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+		else if (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+	}
+	printf("%d\n", n);
+}
