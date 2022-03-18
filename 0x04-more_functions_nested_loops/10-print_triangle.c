8c26ce95b3a56441df4d771550dd1238e14ee607
OsanjoMartine <osanjomartine@gmail.com>
2022-03-17 18:56:50 -0700

    10 triangle andvanced

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_square - print squares
+ * @size: sides
+ *
+ * Return: void
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+	int k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = size -i; j > 0; j--)
+		{
+			_putchar(' ');
+		}
+		for (k = 1; k <= i; k++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
