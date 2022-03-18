0e033a9ab46f28485e6f21b9d8af9c36ed54f8b3
OsanjoMartine <osanjomartine@gmail.com>
2022-03-17 17:21:44 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,9 +14,19 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j + '0');
+			if (j > 9)
+			{
+				_putchar('1');
+			}
+			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
+}
+
+int main(void)
+{
+	more_numbers();
+
+	return (0);
 }
