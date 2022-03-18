92851da64a3dc98bd00f46ad77d6886e972ab15b
OsanjoMartine <osanjomartine@gmail.com>
2022-03-17 17:27:30 -0700

    print dashes on terminal

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_line - print line in the terminal
+ * @n: number of dashes
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		_putchar('-');
+	}
+}
